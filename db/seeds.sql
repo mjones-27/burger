8a2b133355adff1091b0da0ca0ad1ffb24107efa
unknown <mars.jones34@gmail.com>
2020-05-12 22:13:30 -0700

    code working

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,6 +1,6 @@
-INSERT INTO cats (name) VALUES ('Meeses');
-INSERT INTO cats (name) VALUES ('Bobbi');
-INSERT INTO cats (name, sleepy) VALUES ('Sylvester', true);
-INSERT INTO cats (name, sleepy) VALUES ('Marilyn Manson', true);
-INSERT INTO cats (name, sleepy) VALUES ('Joe Biden', true);
-INSERT INTO cats (name) VALUES ('Owens');
+INSERT INTO burgers (name) VALUES ('Tasty');
+INSERT INTO burgers (name) VALUES ('Whopper');
+INSERT INTO burgers (name) VALUES ('Big Kahuna');
+INSERT INTO burgers (name) VALUES ('Big Mac');
+INSERT INTO burgers (name) VALUES ('Quarter Pounder');
+INSERT INTO burgers (name) VALUES ('Impossible');
